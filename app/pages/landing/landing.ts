f8fa1d40417e826556a11616ed3d23e619771800
Pascal Brun <brunpasc@gmail.com>
2016-09-17 04:18:47 +0200

    added landing page & bg asset

---
+++ app/pages/landing/landing.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the LandingPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/landing/landing.html',
+})
+export class LandingPage {
+  constructor(private nav: NavController) {
+    
+  }
+}

3478de5866bf2ae052a6894fd23b8eeb4c517ca4
Andreas Gassmann <andreas@andreasgassmann.ch>
2016-09-17 03:10:37 +0200

    add eddystone plugin

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,7 @@ import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
 
+declare var evothings: any;
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -15,6 +16,34 @@ export class MyApp {
     this.rootPage = TabsPage;
 
     platform.ready().then(() => {
+
+      function foundBeacon(beacon)
+      {
+        // Note that beacon.url will be null until the URL
+        // has been received. Also note that not all Eddystone
+        // beacons broadcast URLs, they may send UIDs only.
+        console.log('Found beacon: ' + beacon.url);
+        console.log('Address: ' + beacon.address);
+        console.log(JSON.stringify(beacon));
+        console.log(beacon);
+      }
+
+      function scanError(error)
+      {
+        console.log('Eddystone scan error: ' + error)
+      }
+
+      evothings.eddystone.startScan(foundBeacon, scanError);
+      /*
+        ble.isEnabled(
+          function() {
+            console.log("Bluetooth is enabled");
+          },
+          function() {
+            console.log("Bluetooth is *not* enabled");
+            alert("Bluetooth is *not* enabled");
+          }
+      );*/
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();

3f3360b7067c48530bd299e53e7ec315bf78fc35
Andreas Gassmann <andreas@andreasgassmann.ch>
2016-09-17 08:33:05 +0200

    Add sample youtube video

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core'
 import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
 import {HistoryPage} from '../history/history';
 import {ChatPage} from '../chat/chat';
 

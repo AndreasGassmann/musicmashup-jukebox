97dc071df6b0446c69bfc353dc40c0bf4cfcb583
Andreas Gassmann <andreas@andreasgassmann.ch>
2016-09-17 16:29:05 +0200

    Add social sharing plugin

--- app/modals/info/info.ts
+++ app/modals/info/info.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
 import { QRCodeComponent } from 'ng2-qrcode';
+import { SocialSharing } from 'ionic-native';
 
 @Component({
   templateUrl: 'build/modals/info/info.html',
   directives: [QRCodeComponent]
 })
 export class InfoModal {
+  roomName: string;
 
   constructor(private viewCtrl: ViewController) {
+    this.roomName = '1';
+  }
 
+  share() {
+    SocialSharing.share("Add and vote for songs to make this party awesome!", "Join my party!", "", "http://google.ch/");
   }
 
   dismiss() {

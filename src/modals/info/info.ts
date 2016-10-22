44e7ed7d3962392ef27ada952181ec9efa3776ff
AndreasGassmann <andreas@andreasgassmann.ch>
2016-10-23 01:23:06 +0200

    fix(leave-room): Reload app when leaving the room

--- src/modals/info/info.ts
+++ src/modals/info/info.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { ViewController, NavParams, NavController } from 'ionic-angular';
+import { AlertController, ViewController, NavParams, NavController } from 'ionic-angular';
 import { SocialSharing } from 'ionic-native';
-import {SocketService} from "../../providers/socket-service";
-import {LandingPage} from "../../pages/landing/landing";
+import { SocketService } from "../../providers/socket-service";
 
 @Component({
   templateUrl: 'info.html'
@@ -10,7 +9,7 @@ import {LandingPage} from "../../pages/landing/landing";
 export class InfoModal {
   roomName: string;
 
-  constructor(private viewCtrl: ViewController, private _navParams: NavParams, private socketService: SocketService, private navController: NavController) {
+  constructor(private viewCtrl: ViewController, private _navParams: NavParams, public alertCtrl: AlertController) {
     this.roomName = this._navParams.get("roomName").toString();
   }
 
@@ -18,12 +17,33 @@ export class InfoModal {
     SocialSharing.share("Add and vote for songs to make this party awesome!", "Join my party!", "", "http://google.ch/");
   }
 
-  leaveRoom() {
-    this.viewCtrl.dismiss();
-    this.socketService.sendPlainMessage('leaveRoom', () => {
-      this.socketService.reset();
-      this.navController.setRoot(LandingPage);
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Leave room',
+      message: 'Do you really want to leave this room?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+            this.leaveRoom();
+          }
+        }
+      ]
     });
+    confirm.present();
+  }
+
+  leaveRoom() {
+    console.log('Leaving room');
+    // TODO: Let server know we are leaving
+    window.location.reload();
   }
 
   dismiss() {

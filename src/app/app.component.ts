906a5912f8f98f53ff2fd60b05d426b379a11c67
AndreasGassmann <andreas@andreasgassmann.ch>
2016-10-25 23:30:27 +0200

    feat(3dtouch): Add 3d touch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { StatusBar, ThreeDeeTouch, ThreeDeeTouchQuickAction, ThreeDeeTouchForceTouch } from 'ionic-native';
 
 import { LandingPage } from "../pages/landing/landing";
 import { SocketService } from "../providers/socket-service";
@@ -14,6 +14,52 @@ export class MyApp {
 
   constructor(platform: Platform, public _socketService: SocketService) {
     platform.ready().then(() => {
+
+      ThreeDeeTouch.isAvailable().then(isAvailable => console.log("3D Touch available? " + isAvailable));
+
+      ThreeDeeTouch.watchForceTouches()
+        .subscribe(
+          (data: ThreeDeeTouchForceTouch) => {
+            console.log("Force touch %" + data.force);
+            console.log("Force touch timestamp: " + data.timestamp);
+            console.log("Force touch x: " + data.x);
+            console.log("Force touch y: " + data.y);
+          }
+        );
+
+      let actions: Array<ThreeDeeTouchQuickAction> = [
+        {
+          type: 'checkin',
+          title: 'Check in',
+          subtitle: 'Quickly check in',
+          iconType: 'Compose'
+        },
+        {
+          type: 'share',
+          title: 'Share',
+          subtitle: 'Share like you care',
+          iconType: 'Share'
+        },
+        {
+          type: 'search',
+          title: 'Search',
+          iconType: 'Search'
+        },
+        {
+          type: 'test',
+          title: 'Show favorites',
+          iconType: 'HeartTemplate'
+        }
+      ];
+      ThreeDeeTouch.configureQuickActions(actions);
+
+      ThreeDeeTouch.onHomeIconPressed().subscribe(
+        (payload) => {
+          // returns an object that is the button you presed
+          console.log('Pressed the ${payload.title} button');
+          console.log(payload.type);
+
+        });
 
       this._socketService.initialize();
       this._socketService.socketService.subscribe(event => {
